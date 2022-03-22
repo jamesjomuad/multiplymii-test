4d1fe31c45516189c4ed6d986fabc043d3430052
james <james.jomuad@outlook.com>
2022-03-22 09:12:41 +0800

    Student details

--- hooks/shortcodes.php
+++ hooks/shortcodes.php
@@ -37,26 +37,20 @@ add_shortcode('axcelerate_student', function () {
 
     $student = axcelerate_student($student_id);
 
-    dump(
-        $student
-    );
-
     ?>
 
     <div class="row gx-4 gy-4 mt-3">
         <div class="col-12 d-flex flex-fill">
             <div class="card w-100">
                 <div class="card-body">
-                    <h5 class="card-title"><?= $student->GIVENNAME ?></h5>
+                    <h4 class="card-title"><?= $student->GIVENNAME ?></h4>
                     <p class="card-text"><?= $student->COMMENT ?></p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Position: <?= $student->POSITION ?></li>
-                    <li class="list-group-item">Email: <?= $student->EMAILADDRESS ?></li>
+                    <?php foreach( $student as $key=>$data) : ?>
+                        <li class="list-group-item"><b><?= $key ?>:</b> <?= $data ?></li>
+                    <?php endforeach; ?>
                 </ul>
-                <div class="card-body">
-                    <a href="/student/<?= $student->CONTACTID ?>" class="btn btn-primary">More</a>
-                </div>
             </div>
         </div>
     </div>
